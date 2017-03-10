40e634848da71fdf176a647e5bb208453142e922
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-03-10 15:47:47 +0800

    add promise

---
+++ http/request.js
@@ -0,0 +1,34 @@
+const host = "***.**.**";
+
+//Promise形式的http请求
+export default function(url, params, type = "GET") {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    var data = new FormData();
+
+    var formdata = function(params) {
+        for (var i in params) {
+            if (args.hasOwnProperty(i)) {
+                data.append(i, args[i]);
+            }
+      }
+    };
+    params ? formdata(params) : null;
+
+    xhr.timeout = 3000;
+    xhr.responseType = "json";
+
+    xhr.open(type, host + url, true);
+    xhr.onload = function (res) {
+      var json = xhr.response;
+      resolve(json);
+    };
+
+    xhr.onerror = function(e) {
+      console.error("网络错误");
+      reject(e);
+    };
+
+    xhr.send(data);
+  });
+}
